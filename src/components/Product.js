aea25ae5d1e80d7516a31b2e846b5c85816967cd
Rikke Blom <rikk957v@stud.kea.dk>
2021-11-15 16:38:43 +0100

    added conditional styling on soldout

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 export default function Product(props) {
   return (
-    <article>
+    <article style={{ backgroundColor: props.product.soldOut ? "rgba(190, 52, 70, 0.507)" : "rgba(73, 52, 190, 0.307)" }}>
       <h2>{props.product.productdisplayname}</h2>
       <p>{props.product.productdescription}</p>
     </article>
