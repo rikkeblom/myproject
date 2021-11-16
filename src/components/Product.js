9500e20466d4d8742639df7a5662bab0ac40ec6e
Rikke Blom <rikk957v@stud.kea.dk>
2021-11-16 13:43:45 +0100

    save before exercise 10

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 export default function Product(props) {
-  function clicked(e) {
-    console.log("'id " + props.product.id + "' was clicked");
+  const [count, setCount] = useState(0);
+
+  function addCount() {
+    setCount((previousCount) => previousCount + 1);
+  }
+
+  function removeCount() {
+    setCount((previousCount) => {
+      if (previousCount > 1) {
+        return previousCount - 1;
+      }
+      return 0;
+    });
   }
 
   return (
     <article style={{ backgroundColor: props.product.soldOut ? "rgba(190, 52, 70, 0.507)" : "rgba(73, 52, 190, 0.307)" }}>
       <h3>{props.product.productdisplayname}</h3>
       <p>{props.product.productdescription}</p>
-      <button onClick={clicked}>buy now</button>
+      <div>
+        <button onClick={removeCount}>-</button>
+        <span>{count}</span>
+        <button onClick={addCount}>+</button>
+      </div>
     </article>
   );
 }
