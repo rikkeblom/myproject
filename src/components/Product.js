0907f73f0724df423e75a0d2e1b47da4e75ae2bd
Rikke Blom <rikk957v@stud.kea.dk>
2021-11-16 12:29:40 +0100

    added button to articles and logs id on click

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,13 @@
 export default function Product(props) {
+  function clicked(e) {
+    console.log("'id " + props.product.id + "' was clicked");
+  }
+
   return (
     <article style={{ backgroundColor: props.product.soldOut ? "rgba(190, 52, 70, 0.507)" : "rgba(73, 52, 190, 0.307)" }}>
-      <h2>{props.product.productdisplayname}</h2>
+      <h3>{props.product.productdisplayname}</h3>
       <p>{props.product.productdescription}</p>
+      <button onClick={clicked}>buy now</button>
     </article>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,6 +3,7 @@ import Product from "./Product.js";
 export default function ProductList(props) {
   return (
     <section className="productList">
+      <h2 style={{ gridColumn: "1/4", marginBottom: "0" }}>Product List</h2>
       <Product product={props.product} />
       <Product product={props.product} />
       <Product product={props.product} />
